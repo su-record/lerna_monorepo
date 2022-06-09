832851f64b2794ccfbc2c91df80caf19cb6da557
willy <willy@clobot.co.kr>
2022-06-09 13:48:20 +0900

    [MOD] 폴더 복사 후 package.json name 수정

--- addedProject.js
+++ addedProject.js
@@ -1,5 +1,5 @@
 const fs = require("fs-extra");
-const exec = require("child_process").exec;
+// const exec = require("child_process").exec;
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
