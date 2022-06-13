06b57566b407a99f27b613db8e145dee1b150569
willy <willy@clobot.co.kr>
2022-06-13 13:54:10 +0900

    [DEL] not used

--- run-dev.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require("child_process");
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question("Run develop project name? ", (name) => {
-  exec("npm run serve", { cwd: `projects/${name}` });
-});
