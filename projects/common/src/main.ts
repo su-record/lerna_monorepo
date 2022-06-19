cc51f393a4733d5a1ae60bfd120f6999c58c5a33
함수원 <dev.code.su@gmail.com>
2022-06-20 00:06:07 +0900

    [MOD] import reset.scss

--- projects/common/src/main.ts
+++ projects/common/src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 
 import "@mdi/font/css/materialdesignicons.min.css";
 import "@/styles/index.css";
-import "@/styles/scss/reset";
-import "@/styles/scss/common";
+import "@/styles/reset.scss";
 
 createApp(App).mount("#app");
