ec4da99ae9c15e116e7875a430bf00ed4a7ca42a
willy <willy@clobot.co.kr>
2022-06-21 16:52:24 +0900

    [ADD] CheckBox - Default

---
+++ projects/common/src/components/CheckBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input
+      type="checkbox"
+      :id="uid"
+      :checked="modelValue === CHECKED_STRING.TRUE"
+      @click="update"
+    />
+    <label :for="uid"></label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUUID } from "../composable/useUUID";
+import { CHECKED_STRING } from "../contants/constant";
+
+const uid = useUUID();
+const props = defineProps({
+  modelValue: String,
+  theme: String,
+});
+const emit = defineEmits<{ (e: string, value: string): void }>();
+const update = function (ev: InputEvent): void {
+  const el = ev.target as HTMLInputElement;
+  emit("update:modelValue", el.checked ? CHECKED_STRING.TRUE : CHECKED_STRING.FALSE);
+};
+</script>
