534b8ea380c6016cb62e2568918b428ba228bddb
willy <willy@clobot.co.kr>
2022-06-22 10:34:43 +0900

    [ADD] storybook main - css loader - alias src

--- projects/common/.storybook/main.js
+++ projects/common/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -14,6 +16,17 @@ module.exports = {
     },
   ],
   framework: "@storybook/vue3",
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+
+    return config;
+  },
   core: {
     builder: "@storybook/builder-webpack5",
   },
