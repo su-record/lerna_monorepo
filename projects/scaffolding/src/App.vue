cfa17370d6c9906ebbcecf6eba7ae315aa955477
함수원 <dev.code.su@gmail.com>
2022-08-01 02:37:24 +0900

    [MOD] vue-cli => vite vue3

--- projects/scaffolding/src/App.vue
+++ projects/scaffolding/src/App.vue
@@ -1,41 +1,85 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-
-  <common-button :label="'common import button'"></common-button>
-  <router-view />
-</template>
-
 <script setup lang="ts">
-import CommonButton from "projects-common/src/components/CommonButton.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
-@import "@mdi/font/css/materialdesignicons.min.css";
-@import "projects-common/src/assets/styles/index.css";
-@import "projects-common/src/assets/styles/reset";
-@import "projects-common/src/assets/styles/common";
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
 nav {
-  padding: 30px;
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
